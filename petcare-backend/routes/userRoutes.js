de4049f9dcd7ea598187347aee6d8ae8c72411d5
Sreckoooo <tadej.sreckovic@gmail.com>
2025-10-16 13:49:00 +0200

    spremembe

--- petcare-backend/routes/userRoutes.js
+++ petcare-backend/routes/userRoutes.js
@@ -47,4 +47,14 @@ router.post('/login', async (req, res) => {
   });
 });
 
+// GET /api/users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find({}, '-password'); // ne vrača gesel
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 export default router;
